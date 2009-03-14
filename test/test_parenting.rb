8d146554c9134d8057906caffb25721750cc3b5b
Ari Lerner <alerner@YPCMC05591.local>
2009-03-14 15:25:15 -0700

    Added test on self

--- test/test_parenting.rb
+++ test/test_parenting.rb
@@ -8,8 +8,9 @@ class QuickieTest < Test::Unit::TestCase
     before do
       @a = Quickie.new do
         @@b = Quickie.new do
+          my_name "bob"
           @@c = Quickie.new do
-            my_name "is_frank"
+            my_name "frank"
           end
         end
       end
@@ -19,11 +20,13 @@ class QuickieTest < Test::Unit::TestCase
       @@b.parent.should == @a
       @a.parent.should == nil
     end
-    it "should set my_name on @@c to is_frank" do
-      @@c.my_name.should == "is_frank"
+    it "should set my_name on @@c to frank" do
+      @@c.my_name.should == "frank"
     end
-    it "should not set my_name to is_frank on @@b or @a" do
-      @@b.my_name.should == nil
+    it "should set my_name on @@b to bob" do
+      @@b.my_name.should == "bob"
+    end
+    it "should not set my_name to is_frank on @a" do
       @a.my_name.should == nil
     end
   end
